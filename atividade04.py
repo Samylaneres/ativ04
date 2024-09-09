65070636a9278460bcebca810371c280157ea989
Samylaneres <samyaneres13@gmail.com>
2024-09-09 10:11:51 -0300

    media

---
+++ atividade04.py
@@ -0,0 +1,9 @@
+# Crie um programa que receba três notas de um aluno e calcule a média aritmética.
+
+N1 = int(input("Sua 1 nota "))
+N2 = int(input("Sua 2 nota"))
+N3 = int (input("Sua 3 nota"))
+
+med = (N1 + N2 + N3)/3
+
+print(F"Sua media é {med}")
